3b3fe80d0bf14995e7799f5565fa9bb9727091de
Goro1311 <94751793+Goro1311@users.noreply.github.com>
2021-11-27 19:17:25 +0300

    Add files via upload

--- bd.py
+++ bd.py
@@ -3,17 +3,14 @@ import sqlite3
 def create_first_table():
     cursor.execute("CREATE TABLE types (id real, type text)")
 
-def add_three_rows():
-    cursor.execute("INSERT INTO types VALUES ('1','Абиссинская кошка')")
-    cursor.execute("INSERT INTO types VALUES ('2','Австралийский мист')")
-    cursor.execute("INSERT INTO types VALUES ('3','Американская жесткошерсная')")
+def delete_type():
+    cursor.execute("UPDATE types SET type = 'Artem' WHERE type = 'Абиссинская кошка'")
 
     
 if __name__ == "__main__":
     conn = sqlite3.connect("my_test.db")
     cursor = conn.cursor()
-    create_first_table()
-    add_three_rows()
+    delete_type()
     conn.commit()
     conn.close()
 
